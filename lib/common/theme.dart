b4fc3355ee5fa7adacc530265bd403b84e3c004c
Soni Julianto <sonijulianto@gmail.com>
2021-11-13 20:06:26 +0700

    add multiple pages

---
+++ lib/common/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blackColor = Color(0xff000000);
+Color whiteColor = Color(0xffffffff);
+Color buttonColor = Color(0xffAEE9F9);
+Color greenColor = Color(0xff74E59F);
+Color blueColor = Color(0xff4572EB);
+Color greyColor = Color(0xff9C9C9C);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: greenColor,
+);
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: blueColor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greyColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight bold = FontWeight.w700;
---
+++ lib/pages/lokasi_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:lomba/common/theme.dart';
+import 'package:lomba/pages/pasar_qna_page.dart';
+import 'package:lomba/widgets/lokasi_card.dart';
+
+class LokasiPage extends StatefulWidget {
+  const LokasiPage({Key? key}) : super(key: key);
+
+  @override
+  _LokasiPageState createState() => _LokasiPageState();
+}
+
+class _LokasiPageState extends State<LokasiPage> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    lowerBound: 0.7,
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeOutBack,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 25, 178, 238),
+                Color.fromARGB(255, 21, 236, 229),
+                Color.fromARGB(255, 255, 236, 229),
+              ],
+            ),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 24,
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: whiteColor,
+                      size: 50,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'PILIH TITIK LOKASI',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              SingleChildScrollView(
+                padding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 40,
+                ),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    LokasiCard(
+                      image: 'assets/pasar.jpg',
+                      description:
+                          'Pasar                                                         ',
+                      animation: _animation,
+                      ontap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PasarQnaPage()));
+                      },
+                    ),
+                    LokasiCard(
+                      image: 'assets/alun.jpg',
+                      description:
+                          'Alun-Alun                                                         ',
+                      animation: _animation,
+                      ontap: () {},
+                    ),
+                    LokasiCard(
+                      image: 'assets/pedesaan.jpg',
+                      description:
+                          'Pedesaan                                                         ',
+                      animation: _animation,
+                      ontap: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              Text(
+                'Jadi Teladan Bermanfaat untuk Indonesia Sehat',
+                style: blackTextStyle.copyWith(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
